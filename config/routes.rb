ec6436645a0dbc9a509d13b613c3cbc66adbed4b
isakawamoto <sayuri.k92@hotmail.com>
2020-07-23 18:31:57 -0300

    Primeira parte do curso da Alura de Rails 5.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ resources :produtos, only: [:new, :create, :destroy]
+ get "produtos/busca", to: "produtos#busca", as: :busca_produto
+ root to: "produtos#index"
 end
