876065277b471f0fcf49f1d77fcbbb95f6509437
isakawamoto <sayuri.k92@hotmail.com>
2020-08-14 17:37:42 -0300

    código refatorado, evitando cópias

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,35 +1,64 @@
 class ProdutosController < ApplicationController
 
+	before_action :set_produto, only: [:edit, :update, :destroy]
+
 	def index
-		@produto = Produto.order :nome
+		@produtos = Produto.order :nome
 		@produto_com_desconto = Produto.order(:preco).limit 1
 	end
 
 	def create
-	    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
-	    @produto = Produto.new valores
+	    @produto = Produto.new produto_params
 	    if @produto.save
 	        flash[:notice] = "Produto salvo com sucesso!"
 	        redirect_to root_url
 	    else
-	        render :new
+	    	renderiza :new
 	    end
 	end
 
 
 	def destroy
-		id = params[:id]
-		Produto.destroy id
+		@produto.destroy
 		redirect_to root_path
 	end
 
 	def busca
 		@nome = params[:nome]
-		@produto = Produto.where "nome like?", "%#{@nome}%"
+		@produtos = Produto.where "nome like?", "%#{@nome}%"
 	end
 
 	def new
 		@produto = Produto.new
-		@departamento = Departamento.all
+		@departamentos = Departamento.all
 	end
+
+	def edit
+		renderiza :edit
+	end
+
+	def update
+		if @produto.update produto_params
+			flash[:notice] = "Produto atualizado com sucesso"
+			redirect_to root_url
+		else
+			renderiza :edit
+		end
+	end
+
+	private
+
+	def produto_params
+		params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+	end
+
+	def set_produto
+		@produto = Produto.find(params[:id])
+	end
+
+	def renderiza(view)
+		@departamentos = Departamento.all
+		render view
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
  resources :departamentos
  post "departamentos/new", to: "departamentos#new"
- resources :produtos, only: [:new, :create, :destroy]
+ resources :produtos, only: [:new, :create, :destroy, :edit, :update]
  get "produtos/busca", to: "produtos#busca", as: :busca_produto
  root to: "produtos#index"
  # teste
